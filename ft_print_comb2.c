f7fb7c16c54d7ae3083972af4b5dd1059c7950c0
leoc01 <leonardocampetti@gmail.com>
2024-10-01 15:56:19 -0300

    add `ft_print_comb2.c`

---
+++ ft_print_comb2.c
@@ -0,0 +1,25 @@
+#include "libft.c"
+
+void	ft_print_comb2(void)
+{
+	int	n1;
+	int	n2;
+
+	n1 = 0;
+	while (n1 <= 98)
+	{
+		n2 = n1 + 1;
+		while (n2 <= 99)
+		{
+			ft_putchar(n1 / 10 + '0');
+			ft_putchar(n1 % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(n2 / 10 + '0');
+			ft_putchar(n2 % 10 + '0');
+			if (!(n1 == 98 && n2 ==99))
+				write(1, ", ", 2);
+			n2++;
+		}
+		n1++;
+	}
+}
